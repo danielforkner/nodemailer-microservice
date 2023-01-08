2073091c7ef522b9f03bf634152db5cc0d3e587e
danielforkner <daniel.forkner@gmail.com>
2023-01-08 13:00:18 -0500

    cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 const { sendMail } = require('./mailFunctions');
@@ -7,6 +8,7 @@ const app = express();
 const router = express.Router();
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use((req, res, next) => {
